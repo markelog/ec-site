1fd6e0b6fe6051af81d555f3d8e08412d3497d27
Oleg Gaidarenko <markelog@gmail.com>
2018-03-20 22:39:50 +0300

    Add redirect app

---
+++ redirect/app.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const { PORT = 3000 } = process.env
+
+http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain')
+  res.setHeader('Location', 'https://eclectica.sh')
+  res.statusCode = 301
+  res.end('Redirecting to ' + 'https://eclectica.sh')
+}).listen(PORT)
