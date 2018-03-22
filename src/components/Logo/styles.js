acef9cc2d0e3e3637f921575f4e3159877bfb631
Oleg Gaidarenko <markelog@gmail.com>
2018-03-22 22:38:51 +0300

    Hopefully, finish with the mobile styles

--- src/components/Logo/styles.js
+++ src/components/Logo/styles.js
@@ -39,38 +39,8 @@ const styles = {
 
   link: `
     margin: 0;
-  	width: 80vh;
+  	width: 50vw;
     display: inline-block;
-
-		@media only screen
-		  and (max-device-width: 760px) {
-				width: 40vh;
-		}
-
-		/* iPhone X portrait */
-		@media only screen
-		  and (min-device-width: 375px)
-		  and (max-device-width: 812px)
-		  and (orientation: portrait) {
-				width: 40vh
-		}
-
-		/* iPhone X landscape */
-		@media only screen
-		  and (min-device-width: 375px)
-		  and (max-device-width: 812px)
-		  and (orientation: landscape) {
-				width: 100vh
-		}
-
-		/* iPad Pro 10.5 Portrait */
-		@media only screen
-			and (min-device-width: 834px)
-			and (max-device-width: 834px)
-			and (orientation: portrait)
-			and (-webkit-min-device-pixel-ratio: 2) {
-				width: 70vh
-		}
   `,
 
   install: {
@@ -90,7 +60,7 @@ const styles = {
   `,
 
   sub: `
-    font-size: 4rem;
+    font-size: 5vw;
     white-space: nowrap;
     font-weight: 400;
     letter-spacing: 2px;
@@ -98,29 +68,6 @@ const styles = {
     margin: 5rem 0 0;
     display: block !important;
     animation: ${fadein} 3s forwards, ${slide} 3s forwards;
-
-		@media only screen
-		  and (max-device-width: 760px) {
-				font-size: 1.1rem;
-		}
-
-		/* iPhone X portrait */
-		@media only screen
-			and (min-device-width: 375px)
-			and (max-device-width: 812px)
-			 {
-				font-size: 1.2rem;
-		}
-
-		/* iPhone X landscape */
-		@media only screen
-			and (min-device-width: 375px)
-			and (max-device-width: 812px)
-			 {
-				font-size: 2rem;
-		}
-
-
   `,
 };
 
--- src/components/Navigation/styles.js
+++ src/components/Navigation/styles.js
@@ -3,8 +3,9 @@ import { minWidth } from '../../variables/dimensions';
 
 export default {
   ul: `
+		font-size: 3.5vw;
     position: absolute;
-    bottom: 100px;
+    bottom: 8vh;
     display: flex;
     justify-content: space-around;
     min-width: ${minWidth};
@@ -12,31 +13,27 @@ export default {
     margin: 0;
 		padding: 0;
 
-    @media (max-width: 500px) {
+    @media (max-width: 600px) {
       flex-direction: column;
       left: 50%;
       transform: translateX(-10%);
       width: auto;
       padding: 3rem 0;
-			bottom: 2rem;
+			bottom: 10vh;
+			font-size: 5vw;
     }
 
-		/* iPhone X portrait */
-		@media only screen
-			and (min-device-width: 375px)
-			and (max-device-width: 812px)
-			and (-webkit-min-device-pixel-ratio: 3)
-			and (orientation: portrait) {
-				bottom: 6rem
+		@media (max-height: 1000px) and
+		(max-width: 3000px) {
+			bottom: 10vh;
 		}
 
-		/* iPhone 6+, 7+ and 8+ */
-		@media only screen
-		  and (min-device-width: 414px)
-		  and (max-device-width: 736px)
-		  and (-webkit-min-device-pixel-ratio: 3)
-		  and (orientation: portrait) {
-				bottom: 4rem
+		@media (max-height: 800px) {
+			bottom: 16vh;
+		}
+
+		@media (max-height: 600px) {
+			bottom: 10vh;
 		}
   `,
 
@@ -51,13 +48,8 @@ export default {
       color: ${dark} !important;
     }
 
-    font-size: 3.5vw;
     text-decoration: none;
     color: white;
-
-    @media (max-width: 650px) {
-      font-size: 1.5rem;
-    }
   `,
 
   active: {
